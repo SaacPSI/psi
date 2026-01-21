40bdc0158e4e434e2dcdce651575580b8d0410e0
aurelien.milliat <aurelien.milliat@imt-atlantique.fr>
2026-01-21 09:58:16 +0100

    Fixe GetAvailableFormats to get all format from the selected camera.

---
+++ Applications/Test/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Microsoft.Psi.Interop.Rendezvous;
+using Microsoft.Psi;
+using Microsoft.Psi.Media;
+using Microsoft.Psi.Media_Interop;
+
+namespace TestingConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //RendezvousServer server = new RendezvousServer(13331);
+            //server.Start();
+            //server.Rendezvous.ProcessAdded += (_, process) =>
+            //{
+            //    Console.WriteLine($"Process added: {process.Name}");
+            //};
+
+            //RendezvousClient client = new RendezvousClient("localhost", 13331);
+            //client.Start();
+            //int i = 0;
+            //while (true)
+            //{
+            //    Console.WriteLine("Press");
+            //    Console.ReadLine();
+            //    client.Rendezvous.TryAddProcess(new Rendezvous.Process($"test{i++}"));
+            //}
+
+            List<string> cameras = MediaCapture.GetAvailableCameras();
+            List<string> CameraCaptureFormat = new List<string>();
+
+            var vals = MediaCapture.GetAvailableFormats(cameras.First());
+            foreach (CaptureFormat format in vals)
+            {
+                CameraCaptureFormat.Add($"{format.nWidth}x{format.nHeight}@{format.nFrameRateNumerator}");
+            }
+        }
+    }
+}
